172cd0df9185595cab60473f08494be6009bb616
DeviPusuluri <DeviPusuluri@_Example_ .com>
2023-08-12 13:09:32 +0000

    Import and Export Multiple Values

---
+++ importMultipleValues.mjs
@@ -0,0 +1,6 @@
+import { myArray } from "./exportMultipleValues.mjs";
+import { bulb } from "./exportMultipleValues.mjs";
+import { multiplyByFour } from "./exportMultipleValues.mjs";
+console.log(myArray);
+console.log(bulb);
+console.log(multiplyByFour(2));
